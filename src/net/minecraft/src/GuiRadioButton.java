825deb8f0e4121254811f11a8f575a27aabbdd92
Irokue <wii-style@hotmail.com>
2012-05-27 15:03:14 +0200

    Oups, j'ai oublié GuiRadioButton

---
+++ src/net/minecraft/src/GuiRadioButton.java
@@ -0,0 +1,80 @@
+package net.minecraft.src;
+
+import net.minecraft.client.Minecraft;
+
+import org.lwjgl.opengl.GL11;
+
+public class GuiRadioButton extends GuiButton{
+
+	private boolean checked = false;
+	private Minecraft mc;
+	private String text;
+	
+	public GuiRadioButton(int par1, int par2, int par3, int par4, int par5,
+			String par6Str) {
+		super(par1, par2, par3, par4, par5, par6Str);
+	}
+	
+	public GuiRadioButton(int id, int xPosition, int yPosition, String text, boolean checked, Minecraft mc){
+		super(id, xPosition, yPosition, 20, 20, "");
+		this.mc = mc;
+		this.text = text;
+		this.checked = checked;
+	}
+
+	 /**
+     * Draws this button to the screen.
+     */
+	  public void drawButton(Minecraft par1Minecraft, int par2, int par3)
+    {
+        if (!drawButton)
+        {
+            return;
+        }
+
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, par1Minecraft.renderEngine.getTexture("/gui/gui.png"));
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        boolean flag = par2 >= xPosition && par3 >= yPosition && par2 < xPosition + width && par3 < yPosition + height;
+        int i = 106;
+        
+        int textColor = 0x444444;
+        
+        if (checked && !flag)
+        {
+            i += height;
+        }
+        
+        if(flag && !checked){
+        	i+= height*2;
+        	textColor = 0x222222;
+        }
+        
+        if(flag && checked){
+        	i+= height*3;
+        	textColor = 0x222222;
+        }
+        
+        
+        drawTexturedModalRect(xPosition, yPosition, 40, i, width, height);
+        mc.fontRenderer.drawString(text, xPosition + 25, yPosition + 6 , textColor);
+    }
+	  
+	/**
+	 * Renvoie la valeur booléenne en entier
+	 * @return valeur booléenne en entier
+	 */
+	public int intValue(){
+		if(checked) return 1;
+		else return 0;
+	}
+
+	public boolean isChecked(){
+		return checked;
+	}
+	
+	public void setChecked(boolean checked){
+		this.checked = checked;
+	}
+	
+
+}
